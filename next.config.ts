e9cb23a414ee6443e9a6403788f70ae4c9348084
amsshk <130871572+amsshk@users.noreply.github.com>
2026-09-21 16:44:33 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+output: 'export',
+basePath: '/giftique',
+images: {
+unoptimized: true,
+},
+}
 
-export default nextConfig;
+export default nextConfig
